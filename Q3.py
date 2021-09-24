5ba30de9235bb56db7952f3b7b35ccb7601182e4
Anup Jacob <L00163455@student.lyit.ie>
2021-09-24 11:22:31 +0100

    First commit third Question with comments

---
+++ Q3.py
@@ -0,0 +1,18 @@
+#-------------------------------------------------
+
+#  Tutorial done by Anup Jacob
+
+#-------------------------------------------------
+
+
+#ppsn     # ppsn number may have characters and numbers(alphanumeric in nature)
+#passport number # passport number may have characters and numbers(alphanumeric in nature)
+#temperature in degrees # degrees have characters and numbers(alphanumeric in nature)
+#cao number # cao number may have characters and numbers(alphanumeric in nature)
+#miles travelled per day # values may have decimal numbers(floating point datatype)
+#Average collection of exam grades
+#File paths # File path will have special characters such as ':','/' which cannot be used as number datatype
+#Person's middle initial
+#Biography Statement #paragraph and may contain long string statments
+#Book Abstract #paragraph and may contain long string statments
+#Mathematical function # mathematical functions will be considered as special characters which cannot be used as number datatype
