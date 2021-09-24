092e3eb0cc2015693f9c8b6e29e1bb08c417d1f7
Anup Jacob <L00163455@student.lyit.ie>
2021-09-24 11:23:44 +0100

    Second commit with comments

--- Q3.py
+++ Q3.py
@@ -10,9 +10,9 @@
 #temperature in degrees # degrees have characters and numbers(alphanumeric in nature)
 #cao number # cao number may have characters and numbers(alphanumeric in nature)
 #miles travelled per day # values may have decimal numbers(floating point datatype)
-#Average collection of exam grades
+#Average collection of exam grades #contains decimal value so probably floating point datatype
 #File paths # File path will have special characters such as ':','/' which cannot be used as number datatype
-#Person's middle initial
+#Person's middle initial # involves Single character only
 #Biography Statement #paragraph and may contain long string statments
 #Book Abstract #paragraph and may contain long string statments
 #Mathematical function # mathematical functions will be considered as special characters which cannot be used as number datatype
