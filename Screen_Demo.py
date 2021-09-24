d9e6eb0f34da6d8f30c9c7dcd189df4ce8a3d009
Anup Jacob <L00163455@student.lyit.ie>
2021-09-24 11:47:20 +0100

    Screen demo third commit with comments

--- Screen_Demo.py
+++ Screen_Demo.py
@@ -5,14 +5,17 @@ bottom_icon = "v"
 left_icon = "<"
 right_icon = ">"
 
-print(top_icon)
+print(top_icon*31)
 
 for i in range(1, 5):
-    print(left_icon)
-    
-print(left_icon + 'Title' + right_icon)
+    print(left_icon +" "*29 + right_icon)
 
-for i in range(1, 5):
-    print(left_icon)
+print(left_icon +" "+ 'Title' +" "*23 + right_icon)
+print(left_icon +" "+ "-"*5 + " "*23 + right_icon)
+print(left_icon +" "+ 'Option a' +" "*20 + right_icon)
+print(left_icon +" "+ 'Option b' +" "*20 + right_icon)
 
-print(bottom_icon)
+for i in range(1, 4):
+    print(left_icon +" "*29 + right_icon)
+
+print(bottom_icon*31)
