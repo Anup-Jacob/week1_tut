8d61162fc336a1a075fc471e3dac87856d7796af
Anup Jacob <L00163455@student.lyit.ie>
2021-09-24 11:08:26 +0100

    Screen demo Second commit with comments

--- Screen_Demo.py
+++ Screen_Demo.py
@@ -1,8 +1,18 @@
+
+
 top_icon = "^"
 bottom_icon = "v"
 left_icon = "<"
 right_icon = ">"
 
 print(top_icon)
+
+for i in range(1, 5):
+    print(left_icon)
+    
 print(left_icon + 'Title' + right_icon)
+
+for i in range(1, 5):
+    print(left_icon)
+
 print(bottom_icon)
