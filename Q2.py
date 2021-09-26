ac223d7f42a737feacc903700ad1115ebe2bb69d
Anup Jacob <L00163455@student.lyit.ie>
2021-09-26 21:45:08 +0100

    Formatted Q2 file

--- Q2.py
+++ Q2.py
@@ -7,15 +7,19 @@ right_icon = ">"
 
 print(top_icon*31)
 
-for i in range(1, 5):
-    print(left_icon +" "*29 + right_icon)
+# for i in range(1, 5):
+print(left_icon +" "*29 + right_icon)
+print(left_icon +" "*29 + right_icon)
 
-print(left_icon +" "+ 'Title' +" "*23 + right_icon)
-print(left_icon +" "+ "-"*5 + " "*23 + right_icon)
-print(left_icon +" "+ 'Option a' +" "*20 + right_icon)
-print(left_icon +" "+ 'Option b' +" "*20 + right_icon)
+print(left_icon +" "*12+ 'Title' +" "*12 + right_icon)
+print(left_icon +" "*29 + right_icon)
+print(left_icon +" "*29 + right_icon)
+print(left_icon +" A "+ 'Option a' +" "*18 + right_icon)
+print(left_icon +" B "+ 'Option b' +" "*18 + right_icon)
+print(left_icon +" C "+ 'Option c' +" "*18 + right_icon)
 
-for i in range(1, 4):
-    print(left_icon +" "*29 + right_icon)
+# for i in range(1, 4):
+print(left_icon +" "*29 + right_icon)
+print(left_icon +" "*29 + right_icon)
 
 print(bottom_icon*31)
